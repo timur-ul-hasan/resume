e32caaf9f047c8718672ab92ab8945ed7b5f2ac4
timur-ul-hasan <hasanjaved065@gmail.com>
2018-12-18 18:14:26 +0500

    Files added

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="http://localhost/cv.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row border-bottom">
+			<div class="col-md-8">
+				<span style="font-size: 600%">Abid Ali</span>
+			</div>
+			<div class="col-md-4">
+				<h3>
+					<i class="fa fa-address-card"></i>
+					<span class="border-bottom">CONTACT</span>
+				</h3>
+				<ul class="border-left list-unstyled lead">
+					<li><i class="fa fa-envelope"></i>abidalixonu@gmail.com</li>
+					<li><i class="fa fa-phone-square"></i>+92 324 8765987</li>
+					<li><i class="fa fa-home"></i>Moh. Rangpura, Sialkot</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-8">
+
+				<p class="blockquote border-bottom">
+					<i class="fa fa-quote-left fa-3x fa-pull-left fa-border"></i>Diligent web developer equipped with technical skills, abilities and a year of experience in hand looking forward seeking an opportunity to work in collaboration with experienced personnel for better career development and professional growth.
+				</p>
+
+				<h3>
+					<i class="fa fa-briefcase"></i>
+					<span class="border-bottom">WORK EXPERIENCE</span>
+				</h3>
+				<ul class="border-left list-unstyled">
+					<li><h4>Karigar Web Solutions</h4></li>
+					<li><small class="border-left" style="padding-left: 5px">Nov 2016 - June 2018</small>
+					<p class="border-bottom lead"><i class="fa fa-user"></i><strong>Jr. Web Developer</strong></p></li>
+					<li class="lead">Developed add-ons in <b>XenForo</b> (A forums community software). Developed and deployed <strong>25+</strong> add-ons for various sites from which a few sites are:</li><br />
+					<li>
+						<table class="table table-striped">
+							<tr>
+								<th>Site Name</th>
+								<th>Forums Link</th>
+							</tr>
+							<tr>
+								<td>PlayerUp</td>
+								<td>https://www.playerup.com</td>
+							</tr>
+							<tr>
+								<td>Tesla Motors Club (TMC)</td>
+								<td>https://teslamotorsclub.com/tmc</td>
+							</tr>
+							<tr>
+								<td>BD Outdoors</td>
+								<td>https://www.bdoutdoors.com/forums</td>
+							</tr>
+							<tr>
+								<td>Coastside Fishing Club</td>
+								<td>https://forums.coastsidefishingclub.com</td>
+							</tr>
+							<tr>
+								<td>Grapplers Guide</td>
+								<td>https://grapplersguide.com</td>
+							</tr>
+							<tr>
+								<td>Overland Bound</td>
+								<td>https://www.overlandbound.com/forums</td>
+							</tr>
+						</table>
+					</li>
+					<li class="lead">Performed SEO on <strong>WordPress</strong> sites and worked on <strong>WordPress</strong> plugins as well.</li>
+				</ul>
+
+				<h3>
+					<i class="fa fa-graduation-cap"></i>
+					<span class="border-bottom">EDUCATION</span>
+				</h3>
+				<ul class="border-left list-unstyled">
+					<li><h4>University of Gujrat (UOG) - <small>Hafiz Hayyat Campus (Main)</small></h4></li>
+					<li><small class="border-left" style="padding-left: 5px">2012 - 2016</small></li>
+					<li class="border-bottom lead"><strong>BS in Software Engineering</strong></li><br>
+					<li class="lead"><b>CGPA:</b> 3.45</li><br>
+					<li class="lead">Followed my passion in university and won competitions that include:</li>
+					<ul class="list-unstyled lead border-left" style="margin-left: 40px">
+						<li>Programming Hunt</li>
+						<li>Speed Programming</li>
+						<li>Code Debug</li>
+					</ul>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<h3>
+					<i class="fa fa-asl-interpreting"></i>
+					<span class="border-bottom">HANDY WITH</span>
+				</h3>
+				<ul class="border-left list-unstyled lead">
+					<li>PHP, OOP, MVC</li>
+					<li>MySql</li>
+					<li>Javascript</li>
+					<li>JQuery</li>
+					<li>Node.js</li>
+					<li>HTML, HTML5</li>
+					<li>CSS, CSS3</li>
+					<li>Bootstrap</li>
+					<li>XenForo</li>
+					<li>Laravel</li>
+					<li>Joomla</li>
+					<li>Wordpress</li>
+					<li>Linux (Ubuntu) Command line</li>
+					<li>Git</li>
+					<li>Slack</li>
+				</ul>
+				<h3>
+					<i class="fa fa-keyboard-o"></i>
+					<span class="border-bottom">TECHNICAL SKILLS</span>
+				</h3>
+				<ul class="border-left list-unstyled lead">
+					<li>Reverse Engineering</li>
+					<li>Code Refactoring</li>
+					<li>Debugging</li>
+					<li>DNS Management</li>
+					<li>cPanel and Server Configuration</li>
+				</ul>
+				<h3>
+					<i class="fa fa-handshake-o"></i>
+					<span class="border-bottom">SOFT SKILLS</span>
+				</h3>
+				<ul class="border-left list-unstyled lead">
+					<li>Communication</li>
+					<li>Teamwork</li>
+					<li>Problem Solving</li>
+					<li>Accountability</li>
+					<li>Adaptability</li>
+					<li>Helpfulness</li>
+				</ul>
+				<h3>
+					<i class="fa fa-users"></i>
+					<span class="border-bottom">REFERENCES</span>
+				</h3>
+				<ul class="list-unstyled lead"><li>Will be available on demand.</li></ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
